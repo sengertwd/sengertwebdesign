6ad2c4074cffd98d27ba082543394391043d0826
Scott Engert <scott@scott-VirtualBox.(none)>
2013-09-09 00:00:02 -0700

    Continued work on Site, added mobile view autoscroll, and also worked on about me and what i do

--- js/site.js
+++ js/site.js
@@ -1,5 +1,9 @@
 $("document").ready(function(){
 
+	setTimeout(function(){
+		window.scrollTo(0,1);
+	}, 0);
+
 	$(".open_mobile").on("click", function(){
 		var $t = $(this),
 			$nav = $("header nav");
